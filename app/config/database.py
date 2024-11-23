caa88a3dddbf463109befdc08e906e8bbb86dbc8
sabrinaksilva <sabrinakappann@gmail.com>
2024-11-23 14:55:08 -0300

    using env vars

--- app/config/database.py
+++ app/config/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config.settings import settings
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/product_db"
+DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ app/config/settings.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_name: str
+
+    class Config:
+        env_file = "app/config/.env"
+
+settings = Settings()
